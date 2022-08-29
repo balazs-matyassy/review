aa8557ae7ab079a7be896846958b29351955c5b8
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-29 18:24:15 +0200

    balazs-matyassy: model

--- src/main/java/hu/progmatic/review/model/Product.java
+++ src/main/java/hu/progmatic/review/model/Product.java
@@ -1,2 +1,81 @@
-package hu.progmatic.review.model;public class Product {
+package hu.progmatic.review.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    // hosszú szövegek tárolása,
+    // rövid szövegnél nem szükséges
+    @Lob
+    private String description;
+
+    private int price;
+
+    // one product to many reviews
+    // CascadeType.ALL
+    // A csomagok is automatikusan felszállnak és leszállnak a repülőről.
+    // orphanRemoval = true -> csomagot nem lehet "eladni", nem lehet új tulajdonosa
+    // review.setProduct(newProduct) törli a reviewt -> nem tud gazdát cserélni
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    public Product() {
+    }
+
+    public Product(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/main/java/hu/progmatic/review/model/Review.java
+++ src/main/java/hu/progmatic/review/model/Review.java
@@ -1,2 +1,76 @@
-package hu.progmatic.review.model;public class Review {
+package hu.progmatic.review.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    // many reviews to one product
+    @ManyToOne
+    private Product product;
+
+    @Lob
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    public Review() {
+    }
+
+    public Review(String description, LocalDateTime createdAt) {
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
+    public Review(Product product, String description, LocalDateTime createdAt) {
+        this.product = product;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", product=" + product +
+                ", description='" + description + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
